68db27bd1bc2a4b2d3bd5e176b4625e685c50ba9
Himanshu Jeena <himanshujeena9718@gmail.com>
2026-06-03 14:16:28 +0530

    feat: clerk webhook - auto save user to database on signup

--- lib/db.ts
+++ lib/db.ts
@@ -1,10 +1,15 @@
 import { PrismaClient } from '@prisma/client'
+import { PrismaNeon } from '@prisma/adapter-neon'
+
+const adapter = new PrismaNeon({
+  connectionString: process.env.DATABASE_URL!
+})
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
 
 export const db =
-  globalForPrisma.prisma ?? new PrismaClient()
+  globalForPrisma.prisma ?? new PrismaClient({ adapter })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db
