5eced0f705727181de6e033b912bfe247463eef7
Himanshu Jeena <himanshujeena9718@gmail.com>
2026-06-03 13:53:02 +0530

    feat: prisma setup with supabase database

---
+++ prisma/migrations/20260603081542_init/migration.sql
@@ -0,0 +1,20 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "clerkId" TEXT NOT NULL,
+    "name" TEXT,
+    "email" TEXT NOT NULL,
+    "college" TEXT,
+    "branch" TEXT,
+    "year" INTEGER,
+    "cgpa" DOUBLE PRECISION,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_clerkId_key" ON "User"("clerkId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
